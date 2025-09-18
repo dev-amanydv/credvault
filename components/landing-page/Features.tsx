60efb897d4ee356811269142da1ee7b57819e6f0
Aman Yadav <ay.work07@gmail.com>
2025-09-19 05:16:30 +0530

    add: README.md

--- components/landing-page/Features.tsx
+++ components/landing-page/Features.tsx
@@ -6,7 +6,7 @@ const Features = () => {
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className='mt-20 flex flex-col justify-center items-center gap-8 animate-fade-in-up animation-delay-200'>
             <div className='grid grid-cols-1 sm:grid-cols-2 justify-between items-center'>
-            <div className="text-white text-3xl sm:text-4xl lg:text-5xl font-medium">An AI-First Video Editor currently in development.</div>
+            <div className="text-white text-3xl sm:text-4xl lg:text-5xl font-medium">How it works?</div>
             </div>
             <hr className='w-full border-0 h-[1px] bg-gradient-to-r from-white/0 via-white/10 to-white/0 mb-2'/>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full'></div>
