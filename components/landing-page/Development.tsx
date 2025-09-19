bcf578338faa2018bee61e19ff567329325c30d9
Aman Yadav <ay.work07@gmail.com>
2025-09-19 13:48:58 +0530

    fix: ui

--- components/landing-page/Development.tsx
+++ components/landing-page/Development.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Development = () => {
   return (
-    <div className='w-full text-7xl font-bold In Development text-neutral-800 fixed top-[700px] h-50  flex justify-center'>
+    <div className='w-full text-7xl font-bold mt-10 text-neutral-800  h-50  flex justify-center'>
       Currently In Development
     </div>
   )
